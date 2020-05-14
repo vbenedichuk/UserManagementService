f89ab008a82194bfcf81cfb46a83651463d7a8d6
Vyacheslav Benedichuk <vbenedichuk@gmail.com>
2020-05-14 18:33:42 +0700

    InitializationException added. Root url updated.

---
+++ src/UserManagementService/Exceptions/InitializationException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace UserManagementService.Exceptions
+{
+    /// <summary>
+    /// Initialization exception.
+    /// </summary>
+    public class InitializationException : Exception
+    {
+
+        /// <summary>
+        /// Initializes new instance of <see cref="InitializationException"/>.
+        /// </summary>
+        /// <param name="message">Error message.</param>
+        public InitializationException(string message) : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes new instance of <see cref="InitializationException"/>.
+        /// </summary>
+        /// <param name="message">Error message.</param>
+        /// <param name="innerException">Inner exception.</param>
+        public InitializationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+
+        /// <summary>
+        /// Initializes new instance of <see cref="InitializationException"/>.
+        /// </summary>
+        /// <param name="info"> The System.Runtime.Serialization.SerializationInfo that holds the serialized object data about the exception being thrown</param>
+        /// <param name="context"> The System.Runtime.Serialization.StreamingContext that contains contextual information about the source or destination.</param>
+        protected InitializationException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
--- src/UserManagementService/Startup.cs
+++ src/UserManagementService/Startup.cs
@@ -159,6 +159,7 @@ namespace UserManagementService
             }
 
             app.UseHttpsRedirection();
+            app.UsePathBase("/auth");
 
             app.UseStaticFiles();
 
